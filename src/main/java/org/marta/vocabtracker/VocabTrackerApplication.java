26c7ec427cde06f6273c4d0e37ee448defc252eb
MKlimecka <m.klimecka.ochedurska@gmail.com>
2026-02-12 22:16:24 +0100

    Add VocabTrackerApplication

---
+++ src/main/java/org/marta/vocabtracker/VocabTrackerApplication.java
@@ -0,0 +1,15 @@
+package org.marta.vocabtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabTrackerApplication {
+
+    public static void main(String[] args)
+
+    {
+        SpringApplication.run(VocabTrackerApplication.class, args);
+    }
+
+}
