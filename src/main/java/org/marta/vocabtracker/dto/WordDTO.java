0641650b27c208766d4988bf2a545eb83c487f33
MKlimecka <m.klimecka.ochedurska@gmail.com>
2026-02-12 15:12:57 +0100

    Add WordDTO  and update Word model

---
+++ src/main/java/org/marta/vocabtracker/dto/WordDTO.java
@@ -0,0 +1,16 @@
+package org.marta.vocabtracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.marta.vocabtracker.model.Status;
+
+import java.util.List;
+@AllArgsConstructor
+@Getter
+@Setter
+public class WordDTO {
+    String original;
+    private List<String> translations;
+    private Status status;
+}
--- src/main/java/org/marta/vocabtracker/model/Word.java
+++ src/main/java/org/marta/vocabtracker/model/Word.java
@@ -1,30 +1,23 @@
 package org.marta.vocabtracker.model;
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
-@Getter
-@Setter
+@Data
+@Builder
 public class Word {
-    private Long id;
+    private UUID id;
     private String original;
     private List<String> translations;
     private Status status;
+    private LocalDateTime createdAt;
 
 
-    public Word(Long id, String original, List<String> translation, Status status) {
-        this.id = id;
-        this.original = original;
-        this.translations = new ArrayList<>();
-        this.status = status;
-    }
 
-    public void addTranslation(String translation) {
-        if (!translations.contains(translation)) {
-            translations.add(translation);
-        }
-    }
 }
